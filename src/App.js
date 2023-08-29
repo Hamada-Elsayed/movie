de0052c23fb760623220ce5f2d7887624082f2e6
Hamada Elsayed <me0099278@gmail.com>
2023-08-29 23:10:17 +0300

    update build for gh-pages

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import Movieslist from './components/Movielist';
 import Navbars from './components/Navbar';
 import { Route,Routes } from 'react-router-dom';
 import Moviedetails from './components/Moviedetails';
+import Landing from './components/Landing.js';
 
 function App() {
   return (
     <div className="App">
     <Navbars/>
+    <Landing/>
+    <Movieslist/>
+
 
     <Routes>
-        <Route path='/' element={ <Movieslist/>} />
         <Route path='movie/:id' element={<Moviedetails/>}/>
     </Routes>
 
